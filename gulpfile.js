27e543f63fadd60c2fbcbd80767c262842adf9b7
Teerayut Hiruntaraporn <teerayut@throughwave.co.th>
2017-03-13 18:01:15 +0700

    Add gulp and quality checking tools

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,27 @@
 var gulp = require('gulp');
-gulp.task('analyze-css', function () {
-  var postcss = require('gulp-postcss');
-  var reporter = require('postcss-reporter');
+var postcss = require('gulp-postcss');
+var stylelint = require('stylelint');
+var reporter = require('postcss-reporter');
 
-  return gulp.src('style.css')
+var autoprefixer = require('autoprefixer');
+var custom_properties = require('postcss-custom-properties')
+var custom_selectors = require('postcss-custom-selectors')
+
+gulp.task('analyze-css', function () {
+  return gulp.src('styles.css')
     .pipe(postcss([
-      reporter()
-    ]));
+      stylelint(),
+      reporter(),
+      autoprefixer(),
+      custom_properties(),
+      custom_selectors()
+    ]))
+    .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('css', function () {
+    return gulp.src('./*.css')
+        .pipe(postcss())
+        .pipe(gulp.dest('./dest'));
 });
