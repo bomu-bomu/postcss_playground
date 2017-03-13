6afa9272d30d29b699f566894849c1c0c4b14f4c
Teerayut Hiruntaraporn <teerayut@throughwave.co.th>
2017-03-13 19:37:15 +0700

    Add immutable and stats

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,33 @@ var autoprefixer = require('autoprefixer');
 var custom_properties = require('postcss-custom-properties')
 var custom_selectors = require('postcss-custom-selectors')
 var doiuse = require('doiuse')
+var immutableCss = require('immutable-css')
+var cssstat = require('postcss-cssstats')
+var listSelector = require('list-selectors').plugin
 
 gulp.task('analyze-css', function () {
-  return gulp.src('styles.css')
+  return gulp.src('*.css')
     .pipe(postcss([
       doiuse({
         browsers: ['ie >= 9', 'last 2 versions']
       }),
+      immutableCss({
+        strict: true
+      }),
       stylelint(),
-      reporter(),
       autoprefixer(),
       custom_properties(),
-      custom_selectors()
+      custom_selectors(),
+      //cssstat(
+      //  function(stats) {
+      //    console.log(stats);
+      //  }
+      //),
+      //listSelector(
+      // { },
+      //  function(list) { console.log(list); }
+      //)
+      reporter(),
     ]))
     .pipe(gulp.dest('./dist'));
 });
